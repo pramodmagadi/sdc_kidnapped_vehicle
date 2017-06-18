53d9b2d3158955bb0f64a3e982fb08f7deb5f3d3
pramodmagadi <pramods286@gmail.com>
2017-06-18 10:30:58 -0400

    Tuned number of particles

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -26,7 +26,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
-    num_particles = 101;
+    num_particles = 9;
     
     normal_distribution<double> x_init(0, std[0]);
     normal_distribution<double> y_init(0, std[1]);
